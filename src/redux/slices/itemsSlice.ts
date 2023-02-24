ba48250d6afc5949e71abeb8523b4df5d4899ddc
Marko Melnychyn <markomelnychyn333@gmail.com>
2023-02-24 15:39:50 +0200

    create-project

---
+++ src/redux/slices/itemsSlice.ts
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+
+const initialState = {
+  totalPrice: 0,
+  items: [],
+};
+
+export const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addItem(state, action) {
+        state.items = action.payload;
+  },
+},
+});
+
+
+export const { addItem} = cartSlice.actions;
+
+export default cartSlice.reducer;
